fd94c096815d14fb0248c42bcfe0c7df012bdb95
Sarveshpal211 <sarvesh7899uyt@gmail.com>
2025-01-20 12:34:30 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  return (
+    <>
+      <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
+        <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2' >
+          <div className='fixed flex flex-wrap justify-center gap-3 bg-white px-2 py-2 rounded-xl'>
+            <button onClick={() => setColor("red")}
+              className='px-2 rounded-full' style={{ backgroundColor: "red" }}>Red</button>
+            <button onClick={() => setColor("blue")} className='px-2 rounded-full' style={{ backgroundColor: "blue" }}>blue</button>
+            <button onClick={() => setColor("green")} className='px-2 rounded-full' style={{ backgroundColor: "green" }}>green</button>
+            <button onClick={() => setColor("yellow")} className='px-2 rounded-full' style={{ backgroundColor: "yellow" }}>yellow</button>
+            <button onClick={() => setColor("black")} className='px-2 rounded-full' style={{ backgroundColor: "black" }}>black</button>
+            <button onClick={() => setColor("maroon")} className='px-2 rounded-full' style={{ backgroundColor: "maroon" }}>maroon</button>
+            <button onClick={() => setColor("pink")} className='px-2 rounded-full' style={{ backgroundColor: "pink" }}>pink</button>
+          </div>
+        </div>
+      </div >
+    </>
+  )
+}
+
+export default App
